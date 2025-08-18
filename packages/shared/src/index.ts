331534267a982c5e944741729a4629d92c972b94
Timofey <timuamua@gmail.com>
2025-08-18 22:13:56 +0300

    up

--- packages/shared/src/index.ts
+++
@@ -1,9 +0,0 @@
-// Типы для DM Copilot
-export * from './types/session'
-export * from './types/tables'
-export * from './types/ai'
-export * from './types/ingestion'
-
-// Утилиты
-export * from './utils/validation'
-export * from './utils/constants'
